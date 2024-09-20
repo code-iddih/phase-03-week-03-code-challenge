db13d98a80065eb2971b6739d4c2b629f2361435
code-iddih <idinyamwaya@gmail.com>
2024-09-20 11:04:40 +0300

    Adds the Venue & Concert Models

--- concerts/models.py
+++ concerts/models.py
@@ -34,3 +34,26 @@ class Band(Base):
         'Concert', 
         back_populates = 'band'
     )
+
+# Venue Model
+class Venue(Base):
+    __tablename__ = 'venues'
+    id = Column(Integer() , primary_key = True)
+    title = Column(String(255) , nullable = False)
+    city = Column(String(255) , nullable = False)
+    # Defining the relationship
+    concerts = relationship(
+        'Concert',
+        back_populates = 'venue'
+    )
+
+# Concert Model
+class Concert(Base):
+    __tablename__ = 'concerts'
+    id = Column(Integer() , primary_key = True)
+    name = Column(String(255) , nullable = False)
+    date = Column(String(50) , nullable = False)
+    band_id = Column(Integer, ForeignKey('bands.id'))
+    venue_id = Column(Integer, ForeignKey('venues.id'))
+    band = relationship('Band', back_populates='concerts')
+    venue = relationship('Venue', back_populates='concerts')
